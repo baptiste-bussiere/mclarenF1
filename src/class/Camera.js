80f35aaed423482023f08fe25223b908b95fb126
Baptiste <70062660+babat-10@users.noreply.github.com>
2023-02-05 22:06:56 +0100

    clean code

--- src/class/Camera.js
+++ src/class/Camera.js
@@ -13,41 +13,29 @@ export default class Camera {
 
         this.setInstance()
         this.setControls()
+
     }
 
     setInstance() {
         this.instance = new THREE.PerspectiveCamera(
-            75,
+            50,
             this.sizes.width / this.sizes.height,
             0.1,
             300
         )
-        this.instance.position.set(-5.00743481376338,
-            1.3622963747003163,
-            4.411647503302046
-        )
+        this.instance.position.set(0, 0, 0)
         this.scene.add(this.instance)
         this.pivot = new THREE.Vector3()
         this.instance.getWorldDirection(this.pivot)
     }
-
     setControls() {
         this.controls = new OrbitControls(this.instance, this.canvas)
         this.controls.enableDamping = true
         this.controls.minDistance = 1
         this.controls.maxDistance = 20
-        this.controls.minPolarAngle = Math.PI / 12
-        this.controls.maxPolarAngle = Math.PI / 2.15
-        this.controls.enablePan = true
-        this.controls.autoRotate = true
-        this.controls.screenSpacePanning = true
-        this.controls.autoRotateSpeed = 0.4
-            // CameraControls.install({ THREE: THREE })
-            // this.cameraControls = new CameraControls(
-            //     this.camera,
-            //     renderer.domElement
-            // )
+
     }
+
 
     resize() {
         this.instance.aspect = this.sizes.width / this.sizes.height
@@ -55,10 +43,6 @@ export default class Camera {
     }
 
     update() {
-        // this.hasControlsUpdated = this.cameraControls.update(delta)
-        // if (this.hasControlsUpdated) {
-        //     renderer.render(scene, camera)
-        // }
 
         this.controls.update()
     }
