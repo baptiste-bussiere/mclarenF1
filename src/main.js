f0217f33122c70146bb18783b3e0a301ab69828e
Baptiste <70062660+babat-10@users.noreply.github.com>
2023-02-05 21:25:45 +0100

    init

--- src/main.js
+++ src/main.js
@@ -3,11 +3,3 @@ import gsap from "gsap"
 import Experience from "./class/Experience.js"
 
 const experience = new Experience(document.querySelector("canvas.webgl"))
-
-function closePopup() {
-    gsap.to(".popup", {
-        opacity: 0,
-        display: "none",
-        zIndex: -1
-    })
-}
